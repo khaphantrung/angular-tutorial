c94936f0d7504ac29e4e6a3e03deeecabc3d2863
kevin.le <kevin.le@terralogic.com>
2018-11-23 15:58:51 +0700

    hero http

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap, filter } from 'rxjs/operators';
+
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
@@ -6,22 +10,74 @@ import { MessageService } from './message.service';
 
 
 
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({
   providedIn: 'root'
 })
 
 export class HeroService {
   
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
 
-  getHeroes(id: number): Observable<Hero[]> {
-  // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched hero');
-    return of(HEROES);
+  constructor(
+     private http: HttpClient,
+    private messageService: MessageService) { }
+  
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
+
+  getHeroes(): Observable<Hero[]> {
+  // TODO: send the message _after_ fetching the heroes
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(heroes => this.log(`fetched ${heroes.length} heroes`)),
+        catchError(this.handleError('getHeroes', []))
+      );
+  }
+
   getHero(id: number): Observable<Hero> {
   // TODO: send the message _after_ fetching the heroes
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    this.log(`fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  updateHero(hero: Hero): Observable<any>{
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap( _ => this.log(`updated hero id= ${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    ) 
+  }
+
+  addHero(hero: Hero): Observable<any>{
+    return this.http.post(this.heroesUrl, hero, httpOptions).pipe(
+      tap( _ => this.log(`added hero w/ id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    ) 
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+  */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
---
+++ src/app/icon-loading/icon-loading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-loading',
+  templateUrl: './icon-loading.component.html',
+  styleUrls: ['./icon-loading.component.css']
+})
+export class IconLoadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
